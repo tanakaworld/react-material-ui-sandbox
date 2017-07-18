1b368acabf74819fad1f11f531b863a84354c9e5
tanakaworld <yutaro.tanaka.world@gmail.com>
2017-07-18 16:05:14 +0900

    Use only IntlPolyfill

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,19 +1,9 @@
 import React, {Component} from 'react';
 import DatePicker from 'material-ui/DatePicker';
-import areIntlLocalesSupported from 'intl-locales-supported';
 import IntlPolyfill from 'intl';
-
-let DateTimeFormat;
-
-if (areIntlLocalesSupported(['ja', 'ja-JP'])) {
-    console.log('locale supported');
-    DateTimeFormat = global.Intl.DateTimeFormat;
-} else {
-    console.log('use polyfill');
-    DateTimeFormat = IntlPolyfill.DateTimeFormat;
-    require('intl/locale-data/jsonp/ja');
-    require('intl/locale-data/jsonp/ja-JP');
-}
+const DateTimeFormat = IntlPolyfill.DateTimeFormat;
+require('intl/locale-data/jsonp/ja');
+require('intl/locale-data/jsonp/ja-JP');
 
 export default class DatePickerSample extends Component {
     render() {
